b69ff81ed402700f817753b2beb48ff34463a65d
durganp1 <durganp1@gmail.com>
2020-10-17 20:18:56 -0500

    add ability to have offline operation

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const FILES_TO_CACHE = [
     './schedule.html',
     './assets/css/style.css',
     './assets/css/bootstrap.css',
-    './assest/css/tickets.css',
+    './assets/css/tickets.css',
     './dist/app.bundle.js',
     './dist/events.bundle.js',
     './dist/tickets.bundle.js',
@@ -21,8 +21,42 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
+            console.log('******************* installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
         })
     )
 })
